3a68e0931fca46e2ec3778797da2d2f8e1155eb5
moohyeah <mohouye@gmail>
2024-08-02 23:44:17 +0800

    keyboard

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 def main() -> None:
     """Run the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("TOKEN").build()
+    application = Application.builder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CallbackQueryHandler(button))
