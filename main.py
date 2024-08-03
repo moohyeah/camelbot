5cc89313bc504d0348ea22222955fc776630f589
moohyeah <mohouye@gmail>
2024-08-03 08:29:49 +0800

    bot

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 # set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 
